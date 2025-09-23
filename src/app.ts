c56402ca7a5c464042f6ee8d837dd5da5214ea6b
Vinisha Yadav <vinishayadav@Mac.lan>
2025-09-23 21:04:42 +0530

    added cors origins

--- src/app.ts
+++ src/app.ts
@@ -43,14 +43,14 @@ const corsOptions = {
 // Use the CORS middleware with your configured options
 app.use(cors(corsOptions));
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
+// app.options("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.sendStatus(200);
+// });
 
-/** ---- Core middleware ---- */
+// /** ---- Core middleware ---- */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /** ---- Routes ---- */
