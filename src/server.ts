559a35a9dc1600d19c6f852850382194cd5391aa
Vinisha Yadav <vinishayadav@Mac.lan>
2025-09-29 16:22:33 +0530

    addded runner

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,15 @@ import "dotenv/config";
 import app from "./app";
 
 const port = Number(process.env.PORT) || 3000;
-const host = process.env.HOST || "0.0.0.0";
+const host = process.env.HOST || "0.0.0.0/0";
 
-// app.listen(port, host, () => {
-//   console.log(`API listening on http://${host}:${port}`);
-// });
-
-const server = app.listen(port, host, () => {
-  const addr = server.address();
-  console.log(
-    `Server listening on http://${(addr as any).address}:${(addr as any).port}`
-  );
+app.listen(port, host, () => {
+  console.log(`API listening on http://${host}:${port}`);
 });
+
+// const server = app.listen(port, host, () => {
+//   const addr = server.address();
+//   console.log(
+//     `Server listening on http://${(addr as any).address}:${(addr as any).port}`
+//   );
+// });
